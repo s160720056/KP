d03a9806f195b4238cc3ef24d69f96befdbf5f7b
00000wkid <xtrac8996@gmail.com>
2023-11-22 20:53:54 +0700

    a

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if(!isset($_SESSION['user'])){
-    header("location:login.php");
-}
+// if(!isset($_SESSION['user'])){
+//     header("location:login.php");
+// }
 
 ?>
 <!DOCTYPE html>
